d2d7f41b57447237862303d8710414461ac74dfb
Daniel Cannon <daniel@danielcannon.co.uk>
2013-11-30 11:39:32 +0000

    Fixed url mapper extension

--- plugin/url_mapper/extractor.go
+++ plugin/url_mapper/extractor.go
@@ -8,7 +8,8 @@ import (
 )
 
 type Extractor struct {
-	values
+	pattern     string
+	replacement string
 }
 
 func (e *Extractor) Id() string {
@@ -16,13 +17,23 @@ func (e *Extractor) Id() string {
 }
 
 func (e *Extractor) Setup(values []Value) error {
-	e.values = values
+	// Validate config
+	if pattern, ok := config["pattern"]; !ok {
+		return errors.New(fmt.Sprintf("The %s extractor must be passed a source url", e.Id()))
+	} else {
+		e.pattern = pattern.(string)
+	}
+
+	if replacement, ok := config["replacement"]; !ok {
+		return errors.New(fmt.Sprintf("The %s extractor must be passed a source url", e.Id()))
+	} else {
+		e.replacement = replacement.(string)
+	}
 
 	return nil
 }
 
 func (e *Extractor) Extract(d *document.Document) (map[string]interface{}, error) {
-	// re := regexp.Compile(e.pattern)
-	// return re.ReplaceAllString(d.Url, e.replacement)
-	return map[string]interface{}{}, nil
+	re := regexp.Compile(e.pattern)
+	return re.ReplaceAllString(d.Url, e.replacement)
 }
